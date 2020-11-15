4eb4fb17555e90bc1f52e04f8d7b56301d514192
amir1004 <amir1004@gmail.com>
2020-11-15 10:05:02 +0200

    123

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use  App\Models\Teacher,App\Models\Student;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleWare('auth:api')->except(["login",'loginStudentOrTeacher']);
+    }
+
+    public function login()
+    {   
+        
+        $credentials= request(['email','password']);
+
+         if(!$token = auth()->attempt($credentials))
+            return $this->invalidCredentials();
+
+       return   $this->respondWithToken($token);
+
+
+    }
+
+    private function respondWithToken($token)
+    {
+        
+        return response()->json(
+            ['token' =>  $token ,
+            'access_type' => 'bearer' ,
+            'expires_in' => auth('api')->factory()->getTTL() * 60 ]);
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['msg' =>  'User Successfully logged out'] );
+    }
+
+    public function refresh()
+    {
+        return  $this->respondWithToken(auth()->refresh() );   
+
+    }
+
+    public function me()
+    {
+        return  $this->response()->json(auth()->user() );   
+
+    }
+
+    public function loginStudentOrTeacher( Request $request)
+    {
+        
+        $member =   Teacher::where('email',  $request->email )->first() ?: Student::where('email',  $request->email )->first() ;
+
+        if(!$member || !$this->validatePasswordMember($member , $request->password)) return $this->invalidCredentials();
+
+        return $member;
+
+    }
+    public function invalidCredentials()
+    {
+        return response()->json(['error' => 'Invalid credentials', 401 ]);
+    }
+    public function validatePasswordMember($model , $password )
+    {
+        return Hash::check($password,  $model->password) ;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -27,3 +27,22 @@ Route::resource('periods', App\Http\Controllers\API\PeriodAPIController::class);
 
 
  
+Route::group(
+    [
+        'middleware' => 'api',
+        'namespace' => 'App\Http\Controllers',
+    ],
+    function ($router) {
+
+     Route::get('/ccache', function() { 
+         $exitCode = Artisan::call('config:clear');
+         $exitCode = Artisan::call('cache:clear');
+         $exitCode = Artisan::call('config:cache'); return 'DONE';
+         //Return anything
+        });
+      Route::post('login', [AuthController::class, 'login']);
+      Route::post('logout',[ AuthController::class ,'logout']);
+      Route::post('refresh', [AuthController::class ,'refresh']);
+      Route::post('me', [AuthController::class ,'me']);
+      Route::post('login-members', [AuthController::class ,'loginStudentOrTeacher']);
+});
