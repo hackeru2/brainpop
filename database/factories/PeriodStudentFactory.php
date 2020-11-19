ba0c1fce251b079de85c7ab9643468e0dc96ce72
amir1004 <amir1004@gmail.com>
2020-11-19 09:17:34 +0200

    o b raw

--- database/factories/PeriodStudentFactory.php
+++ database/factories/PeriodStudentFactory.php
@@ -24,8 +24,8 @@ class PeriodStudentFactory extends Factory
     public function definition()
     {
         return [
-            'student_id' => Student::select('id')->orderByRaw("RAND()")->first()->id,
-            'period_id' => Period::select('id')->orderByRaw("RAND()")->first()->id,
+            'student_id' => Student::select('id')->orderByRaw("RANDOM()")->first()->id, //rand() in sql
+            'period_id' => Period::select('id')->orderByRaw("RANDOM()")->first()->id,
         ];
     }
 }
